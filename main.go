2f6b5e179dc2289a7eb2effe1ecb5537610d61d9
SpiceMAX <88837535+SpiceMAX@users.noreply.github.com>
2022-03-14 10:47:40 +0300

    db

--- main.go
+++ main.go
@@ -15,11 +15,26 @@ func setHandlers(mux *http.ServeMux) {
 	mux.HandleFunc("/employee/create", createEmp)
 	mux.HandleFunc("/employee/remove", removeEmp)
 	mux.HandleFunc("/employee/update", updateEmp)
+	mux.HandleFunc("/tables/create", checkTables)
 }
 
 func connectDB() (db *sql.DB, err error) {
 	db, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	return
+}
+
+func checkTables(w http.ResponseWriter, r *http.Request) {
+	db, err := connectDB()
+	if err != nil {
+		log.Fatal("Ошибка открытия соединения с БД")
+	}
+	defer db.Close()
+
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS public.departments\n(\n name character varying(20) COLLATE pg_catalog.\"default\" NOT NULL,\n phone character varying(12) COLLATE pg_catalog.\"default\",\n CONSTRAINT departments_pkey PRIMARY KEY (name)\n)")
+	if err != nil {
+		log.Println("Ошибка создания таблицы департаментов")
+		http.Error(w, "Внутрення ошибка сервера", 500)
+	}
 }
 
 func main() {
